ab955bba3d83cc9899df00ccf33cebfe9e550442
ANAYAT-AI <anayatbot207@gmail.com>
2025-08-07 18:01:14 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -35,8 +35,8 @@ async (conn, mek, m, { from, sender, reply }) => {
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃',
+                    newsletterJid: '120363420933039839@newsletter',
+                    newsletterName: '𝑨𝑵𝑨𝒀𝑨𝑻-𝑨𝑰',
                     serverMessageId: 143
                 }
             }
