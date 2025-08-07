233f21cc46e8808ca0f2a2c060df152e7f9b64b9
ANAYAT-AI <anayatbot207@gmail.com>
2025-08-07 18:00:18 +0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -55,8 +55,8 @@ cmd({
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363348739987203@newsletter',
-          newsletterName: 'ArslanMD Official',
+          newsletterJid: '120363420933039839@newsletter',
+          newsletterName: 'ANAYAT-AI Official',
           serverMessageId: 143
         }
       }
