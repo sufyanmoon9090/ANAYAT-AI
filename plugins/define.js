46b22889d6c0cf51d359462053b4adf86d1e786f
ANAYAT-AI <anayatbot207@gmail.com>
2025-08-07 17:54:21 +0500

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 ✍️ *Example*: ${example}  
 📝 *Synonyms*: ${synonyms}  
 
-🔗 *Powered By Arslan_MD*`;
+🔗 *Powered By ANAYAT-AI*`;
 
         if (audio) {
             await conn.sendMessage(from, { audio: { url: audio }, mimetype: 'audio/mpeg' }, { quoted: mek });
