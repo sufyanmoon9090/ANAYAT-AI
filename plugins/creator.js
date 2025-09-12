f0c72a94a1bd5b39ca74a2da0081e7c47a040674
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 03:12:30 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -13,9 +13,9 @@ async (conn, mek, m, { from, sender, reply }) => {
     try {
         // Owner information (you can modify these values)
         const ownerInfo = {
-            name: "𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓",
-            number: "+923237045919",
-            photo: "https://files.catbox.moe/lcpy9f.jpg",
+            name: "𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈",
+            number: "+923452401207",
+            photo: "https://files.catbox.moe/rexuf8.jpg",
             bio: "The creator of this amazing bot"
         };
 
@@ -48,7 +48,7 @@ async (conn, mek, m, { from, sender, reply }) => {
     } catch (e) {
         console.error("Creator Command Error:", e);
         // Fallback text if image fails
-        await reply(`👑 *Creator Info*\n\nName: 𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓\nNumber: +923237045919\n\nContact for bot support!`);
+        await reply(`👑 *Creator Info*\n\nName: 𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈\nNumber: +923452401207\n\nContact for bot support!`);
     }
 });
 
