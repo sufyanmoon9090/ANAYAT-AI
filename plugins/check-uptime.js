d227ea4fa01694ab5f44cd6ca5a717c201498021
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 02:58:25 +0500

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -119,8 +119,8 @@ ${config.DESCRIPTION}`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: config.OWNER_NAME || '𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓',
+                    newsletterJid: '120363420933039839@newsletter',
+                    newsletterName: config.OWNER_NAME || '𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈',
                     serverMessageId: 143
                 }
             }
