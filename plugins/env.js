86301293468f295e4c56185b5eeaf78db79e46a0
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 03:22:52 +0500

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -59,8 +59,8 @@ async (conn, mek, m, { from, sender, pushname, reply }) => {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363348739987203@newsletter',
-                        newsletterName: '𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓',
+                        newsletterJid: '120363420933039839@newsletter',
+                        newsletterName: '𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈',
                         serverMessageId: 143
                     }
                 }
