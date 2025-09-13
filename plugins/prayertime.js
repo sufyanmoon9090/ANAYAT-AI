e4c9be4e985fb9e51844be99c40207d079a21d33
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 12:47:43 +0500

    Update prayertime.js

--- plugins/prayertime.js
+++ plugins/prayertime.js
@@ -53,15 +53,15 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/lcpy9f.jpg` },
+                image: { url: `https://files.catbox.moe/rexuf8.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363348739987203@newsletter',
-                        newsletterName: '𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓',
+                        newsletterJid: '120363420933039839@newsletter',
+                        newsletterName: '𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈',
                         serverMessageId: 143
                     }
                 }
