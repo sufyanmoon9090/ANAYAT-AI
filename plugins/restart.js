beb516f665abf7e248b5ac146beff5a598911795
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 12:54:32 +0500

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart Arslan-MD",  
+    desc: "Restart Anayat-AI",  
     category: "owner",  
     filename: __filename  
 },  
