cba58e203dbbba61ea3cfa5353b1b0ca8061ab90
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 13:02:39 +0500

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ ARSLAN┃ MD ✪──";
+        teks += "└──✪ ANAYAT┃ AI ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
