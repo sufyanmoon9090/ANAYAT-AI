f04b8f1c12c429710fb3ad2e042a7951ba3382c4
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-13 10:59:59 +0500

    Update gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -27,7 +27,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 👥 *Followers*: ${data.followers} | Following: ${data.following}
 📅 *Created At*: ${new Date(data.created_at).toDateString()}
 🔭 *Public Gists*: ${data.public_gists}
-> *𝓐𝓻𝓼𝓵𝓪𝓷_𝓜𝓓*`;
+> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*`;
           const sentMsg = await conn.sendMessage(from,{image:{url: data.avatar_url },caption: userInfo },{quoted:mek })
     } catch (e) {
         console.log(e);
