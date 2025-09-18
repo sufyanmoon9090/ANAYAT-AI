d3d6f19ff08daf7d2b9ce23d08156accc55652ce
ANAYAT-AI <anayatbot207@gmail.com>
2025-09-18 23:13:10 +0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -43,7 +43,7 @@ cmd({
     const fileName = contentDisposition ? contentDisposition.match(/filename=(.*)/)[1] : `${repo}.zip`;
 
     // Notify user of the download
-    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*`);
+    reply(`📥 *Downloading repository...*\n\n*Repository:* ${username}/${repo}\n*Filename:* ${fileName}\n\n> *𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙰𝙽𝙰𝚈𝙰𝚃-𝙰𝙸 🫀*`);
 
     // Send the zip file to the user with custom contextInfo
     await conn.sendMessage(from, {
